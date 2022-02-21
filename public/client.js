9e420baaebe7afbb89416f3538f28f1eeb9aec9c
msyed95 <62378802+msyed95@users.noreply.github.com>
2022-02-21 18:29:52 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
-import Stats from '../jsm/libs/stats.module.js';
+import Stats from 'https://unpkg.com/three@0.127.0/examples/jsm/libs/stats.module.js';
 
 // all tools are exported from the "all" file 
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "../gsap/all.js";
